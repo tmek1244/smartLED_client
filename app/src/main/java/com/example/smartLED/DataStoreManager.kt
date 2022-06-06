50c243580ade5683c4acf2bd97779d1fbe891238
tmek1244 <tmek1244@wp.pl>
2022-06-07 01:07:28 +0200

    Save color to flash using preferencesDataStore

---
+++ app/src/main/java/com/example/smartLED/DataStoreManager.kt
@@ -0,0 +1,32 @@
+package com.example.smartLED
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.*
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore("settings")
+
+class DataStoreManager(private val context: Context) {
+    private object PreferencesKeys {
+        val COLOR = intPreferencesKey("color")
+    }
+
+    suspend fun saveToDataStore(value: Int) {
+        context.dataStore.edit {
+            it[PreferencesKeys.COLOR] = value
+        }
+    }
+
+    val colorFlow: Flow<Int> = context.dataStore.data
+        .map { preferences ->
+            // Get our show completed value, defaulting to false if not set:
+            val color = preferences[PreferencesKeys.COLOR] ?: 0
+            color
+        }
+}
+
+
+
--- app/src/main/java/com/example/smartLED/MainActivity.kt
+++ app/src/main/java/com/example/smartLED/MainActivity.kt
@@ -1,84 +1,54 @@
 package com.example.smartLED
 
-import android.content.Context
 import android.content.res.ColorStateList
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import androidx.activity.ComponentActivity
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.graphics.ColorUtils
-import androidx.core.graphics.blue
-import androidx.core.graphics.green
-import androidx.core.graphics.red
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.preferencesDataStore
-import com.example.smartLED.ui.theme.SmartLEDTheme
 import com.madrapps.pikolo.ColorPicker
 import com.madrapps.pikolo.listeners.SimpleColorSelectionListener
 import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.collect
 import java.net.DatagramPacket
 import java.net.DatagramSocket
 import java.net.InetAddress
 import java.nio.ByteBuffer
-import java.util.prefs.Preferences
 
-class MainActivity : ComponentActivity() {
-    var brightness: Int = 50
-//    private  color: Int = 150.0
-    var isOn: Boolean = false
-//     pref: SharedPreferences = null
-
-    private val Context.dataStore: DataStore<Preferences> by preferencesDataStore("settings")
-
+class MainActivity : AppCompatActivity() {
+    private var currentColor: Int = 0
+    private val dataStoreManager: DataStoreManager by lazy { DataStoreManager(this) }
+    private val scope = CoroutineScope(Dispatchers.IO)
 
     override fun onCreate(savedInstanceState: Bundle?) {
-//        val sharedPref = this.getPreferences(Context.MODE_PRIVATE) ?: return
-//        brightness = sharedPref.getInt("BRIGHTNESS", 0)
-//        color = sharedPref.getInt("COLOR", 0x000000).toFloat()
-        val pref = applicationContext.getSharedPreferences("MyPref", Context.MODE_PRIVATE)
-        val color = pref.getInt("COLOR", 0)
-        Log.d("RED READ", color.toString())
-
-        dataStore = applicationContext.createDataStore()
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout)
 
         val colorPicker: ColorPicker = findViewById(R.id.mainColorPicker)
         val colorBox: View = findViewById(R.id.colorBox)
 
-
-//        colorPicker.setColor(ColorUtils.HSLToColor(floatArrayOf(color.toFloat(), 1F, 0.5F)))
-        colorPicker.setColor(color)
-//        colorPicker.backgroundTintList = ColorStateList.valueOf(color)
-//        colorBox.setBackgroundColor()
-//        colorBox.backgroundTintList = ColorStateList.valueOf(ColorUtils.HSLToColor(floatArrayOf(color, 1F, 0.5F)))
-        colorBox.backgroundTintList = ColorStateList.valueOf(color)
+        scope.launch {
+            val color = dataStoreManager.colorFlow
+            color.collect {
+                colorPicker.setColor(it)
+                colorBox.backgroundTintList = ColorStateList.valueOf(it)
+                currentColor = it
+                Log.d("onCreate", "Color: $it")
+            }
+        }
 
         colorPicker.setColorSelectionListener(object : SimpleColorSelectionListener() {
             override fun onColorSelected(color: Int) {
 //                colorBox.setBackgroundColor(color)
+                currentColor = color
                 colorBox.backgroundTintList = ColorStateList.valueOf(color)
                 val hslColor: FloatArray = FloatArray(3)
                 ColorUtils.colorToHSL(color, hslColor)
-                Log.d("RED", color.red.toString())
-                Log.d("GREEN", color.green.toString())
-                Log.d("BLUE", color.blue.toString())
-                Log.d("COLOR", color.toString())
+//                Log.d("RED", color.red.toString())
+//                Log.d("GREEN", color.green.toString())
+//                Log.d("BLUE", color.blue.toString())
+//                Log.d("COLOR", color.toString())
 
-//                val pref = applicationContext.getSharedPreferences("MyPref", Context.MODE_PRIVATE)
-
-                with (pref.edit()) {
-                    putInt("COLOR", color)
-                    commit()
-                }
-
-//                Log.d("Saturation", hslColor[1].toString())
-//                Log.d("Hue", hslColor[0].toString())
-//                colorText.text = color.toString()
                 CoroutineScope(Dispatchers.IO).launch {
                     kotlin.runCatching {
 //                        val serverSocket = DatagramSocket()
@@ -88,29 +58,29 @@ class MainActivity : ComponentActivity() {
                         val message = ByteBuffer.allocate(4).putInt(color).array()
                         val serverSocket = DatagramSocket()
                         serverSocket.broadcast = true
-                        serverSocket.send(DatagramPacket(message, message.size, InetAddress.getByName("192.168.0.80"), 3333))
+                        serverSocket.send(
+                            DatagramPacket(
+                                message,
+                                message.size,
+                                InetAddress.getByName("192.168.0.80"),
+                                3333
+                            )
+                        )
                         serverSocket.close()
 
                     }
                 }
-
-
             }
         })
 
     }
-}
 
+    override fun onPause() {
+        Log.d("onPause", "Saving color...")
+        scope.launch {
+            dataStoreManager.saveToDataStore(currentColor)
+        }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    SmartLEDTheme {
-        Greeting("Android")
+        super.onPause()
     }
 }
